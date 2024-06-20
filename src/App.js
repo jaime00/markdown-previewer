d7e6b7e73ab121c3b09d71413a8dcf204499862a
Jaime <jtorres6100@gmail.com>
2024-06-20 04:40:50 -0500

    stable version of the project complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import MarkdownEditor from './components/MarkdownEditor';
+import RenderViewSide from './components/RenderViewSide';
+import { getCodeFromLocalStorage, getThemeFromLocalStorage, saveCodeToLocalStorage, saveThemeToLocalStorage } from './utils/localStorage';
+
 import './App.css';
+import Selector from './components/Selector';
+import { THEMES } from './constants/theme';
+import { FONT_SIZES } from './constants/font-sizes';
+import SaveAsButton from './components/SaveAsButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [markdownContent, setMarkdownContent] = useState(getCodeFromLocalStorage());
+	const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+	function handleChange(value) {
+		setMarkdownContent(value);
+		saveCodeToLocalStorage(value);
+	}
+	const handleChangeTheme = (prop, value) => {
+		setTheme({ ...theme, [prop]: value });
+		saveThemeToLocalStorage(prop, value);
+	};
+
+	return (
+		<>
+			<h5 className="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Markdown previewer</h5>
+			<section className="flex items-center mb-3">
+				<Selector currentValue={theme.name} handleChange={(e) => handleChangeTheme('name', e.target.value)} optionList={THEMES} label={'Theme'} />
+				<Selector currentValue={theme.fontSize} handleChange={(e) => handleChangeTheme('fontSize', e.target.value)} optionList={FONT_SIZES} label={'Font size'} />
+				<SaveAsButton markdownContent={markdownContent} />
+			</section>
+			<section className="flex">
+				<MarkdownEditor theme={theme} markdownContent={markdownContent} handleChange={handleChange} />
+				<RenderViewSide markdownContent={markdownContent} />
+			</section>
+		</>
+	);
 }
-
-export default App;
